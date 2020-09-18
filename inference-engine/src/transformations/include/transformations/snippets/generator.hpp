66886a0e88035288dc09689791f5dd12ea7661eb
marina kolpakova a.k.a. geexie <marina.kolpakova@intel.com>
2020-09-19 02:09:54 +0300

    [SS] remove POC generator interface

--- inference-engine/src/transformations/include/transformations/snippets/generator.hpp
+++ inference-engine/src/transformations/include/transformations/snippets/generator.hpp
@@ -57,35 +57,6 @@ public:
     // FIXME: exclude extension form opset to use Standart broadcast
     // FIXME: vec shouldn't be here in such an explicit way, but be need to generate tables once for a pass so cannot duplicate body
     // generate it like normal compilers usually do in future
-    virtual void emit(const std::shared_ptr<opset1::Broadcast>& broadcast, RegInfo& registers) const = 0;
-    virtual void emit(const std::shared_ptr<op::FakeBroadcast>& op, RegInfo& registers) const = 0;
-
-    // FIXME: to jitters
-    virtual void emit(const std::shared_ptr<op::Scalar>& op, RegInfo& registers) const = 0;
-
-    virtual void emit(const std::shared_ptr<op::Load>& op, RegInfo& registers) const = 0;
-    virtual void emit(const std::shared_ptr<op::ScalarLoad>& op, RegInfo& registers) const = 0;
-    virtual void emit(const std::shared_ptr<op::BroadcastLoad>& op, RegInfo& registers) const = 0;
-
-    virtual void emit(const std::shared_ptr<op::Store>& op, RegInfo& registers) const = 0;
-    virtual void emit(const std::shared_ptr<op::ScalarStore>& op, RegInfo& registers) const = 0;
-
-    virtual void emit(const std::shared_ptr<opset1::Parameter>& op, RegInfo& registers) const = 0;
-    virtual void emit(const std::shared_ptr<opset1::Result>& op, RegInfo& registers) const = 0;
-
-    virtual void emit(const std::shared_ptr<opset1::Add>& op, RegInfo& registers) const = 0;
-    virtual void emit(const std::shared_ptr<opset1::Subtract>& op, RegInfo& registers) const = 0;
-    virtual void emit(const std::shared_ptr<opset1::Multiply>& op, RegInfo& registers) const = 0;
-    virtual void emit(const std::shared_ptr<opset1::Negative>& op, RegInfo& registers) const = 0;
-    virtual void emit(const std::shared_ptr<opset1::Erf>& op, RegInfo& registers) const = 0;
-    virtual void emit(const std::shared_ptr<opset1::Divide>& op, RegInfo& registers) const = 0;
-    virtual void emit(const std::shared_ptr<opset1::Clamp>& op, RegInfo& registers) const = 0;
-    virtual void emit(const std::shared_ptr<opset1::Relu>& op, RegInfo& registers) const = 0;
-    virtual void emit(const std::shared_ptr<opset1::Sigmoid>& op, RegInfo& registers) const = 0;
-    virtual void emit(const std::shared_ptr<opset1::SquaredDifference>& op, RegInfo& registers) const = 0;
-    virtual void emit(const std::shared_ptr<opset1::Power>& op, RegInfo& registers) const = 0;
-    virtual void emit(const std::shared_ptr<opset1::PRelu>& op, RegInfo& registers) const = 0;
-    virtual void emit(const std::shared_ptr<opset1::Tanh>& op, RegInfo& registers) const = 0;
 
     // FIXME: it's something which should be done automatically with constant map, since it's architecture specific
     virtual void emit_table(const std::shared_ptr<op::Scalar>& constant) const = 0;
